1f44c20a91499b260f6edc2c46796875f35abdd9
Francis James Tolentino <francistolentino1107@gmail.com>
2024-12-23 16:15:27 +0800

    Created Monitor Locations toggle

--- client/app/map/_components/AdminControls.tsx
+++ client/app/map/_components/AdminControls.tsx
@@ -1,7 +1,21 @@
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
+import { useMapContext } from "@/hooks/use-map";
+
 export default function AdminControls() {
+	const { monitorLocations, toggleMonitorLocations } = useMapContext();
+
 	return (
-		<div className="py-6">
-			<p>dfasdfs</p>
+		<div className="py-6 h-full flex flex-col justify-content">
+			<h2 className="text-md">Owners List</h2>
+			<div>
+				<div className="flex justify-between items-center my-2 border rounded-md p-3">
+					<Label className="ml-3" htmlFor="monitorLocations">
+						Monitor Locations
+					</Label>
+					<Switch checked={monitorLocations} id="monitorLocations" onCheckedChange={toggleMonitorLocations} />
+				</div>
+			</div>
 		</div>
 	);
 }
