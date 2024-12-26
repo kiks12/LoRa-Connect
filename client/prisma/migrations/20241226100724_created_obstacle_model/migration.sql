4bba038d8c3893da52378a2e14f44475747d82a9
Francis James Tolentino <francistolentino1107@gmail.com>
2024-12-26 18:08:00 +0800

    New Migration: Created Obstacle Model

---
+++ client/prisma/migrations/20241226100724_created_obstacle_model/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `Obstacle` (
+    `obstacleId` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `dateTime` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `type` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `longitude` DOUBLE NOT NULL,
+    `latitude` DOUBLE NOT NULL,
+
+    PRIMARY KEY (`obstacleId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
