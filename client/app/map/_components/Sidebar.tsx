e7376a7e3c2e009b60abbf66d4db632672f7d91c
Francis James Tolentino <francistolentino1107@gmail.com>
2024-12-29 23:47:44 +0800

    Restructuring of map page for toggleable sidebar

--- client/app/map/_components/Sidebar.tsx
+++ client/app/map/_components/Sidebar.tsx
@@ -2,60 +2,63 @@
 
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
-import { ArrowLeft } from "lucide-react";
+import { activeTab, SIDEBAR_TABS, useSidebarContext } from "@/hooks/use-sidebar";
+import { ArrowLeft, ChevronLeft, ChevronRight, X } from "lucide-react";
 import Link from "next/link";
-import { ReactNode, useState } from "react";
-import OwnersControls from "./OwnersControls";
-import TasksControls from "./TasksControls";
-import AdminControls from "./AdminControls";
-import RescuersControls from "./RescuersControls";
-
-export type activeTab = "ADMIN" | "TASKS" | "OWNERS" | "RESCUERS";
-const tabs: { [key: string]: ReactNode } = {
-	ADMIN: <AdminControls />,
-	TASKS: <TasksControls />,
-	OWNERS: <OwnersControls />,
-	RESCUERS: <RescuersControls />,
-};
 
 export function SideBar() {
-	const [active, setActive] = useState<{ key: activeTab; controls: ReactNode }>({
-		key: "ADMIN",
-		controls: tabs["ADMIN"],
-	});
-
-	function onListClick(newActive: activeTab) {
-		setActive({ key: newActive, controls: tabs[newActive] });
-	}
+	const { active, onListClick, toggleSidebar, open, component } = useSidebarContext();
 
 	return (
-		<div className="flex">
-			<Card className="h-screen w-full p-6 shadow-none rounded-none flex flex-col">
-				<div className="flex items-center">
-					<Link href="/">
-						<Button size="icon" variant="outline" className="rounded-full">
-							<ArrowLeft />
-						</Button>
-					</Link>
-					<h1 className="ml-2 text-xl font-semibold tracking-tight">Control Panel</h1>
+		<>
+			{open ? (
+				<div className="flex w-full justify-between">
+					<Card className="h-screen w-full p-6 shadow-none rounded-none flex flex-col">
+						<div className="flex items-center justify-between">
+							<div className="flex items-center">
+								<Link href="/">
+									<Button size="icon" variant="outline" className="rounded-full">
+										<ArrowLeft />
+									</Button>
+								</Link>
+								<h1 className="ml-2 text-xl font-semibold tracking-tight">Control Panel</h1>
+							</div>
+							<div>
+								<Button size="icon" variant="ghost" onClick={toggleSidebar}>
+									<ChevronRight />
+								</Button>
+							</div>
+						</div>
+						<div className="flex-1">{active.controls}</div>
+					</Card>
+					<Card className="h-screen py-6 w-24 lg:w-36 shadow-none rounded-none">
+						<ul>
+							{Object.keys(SIDEBAR_TABS).map((tab, index) => {
+								return (
+									<li
+										onClick={() => onListClick(tab as activeTab)}
+										key={index}
+										className={`flex justify-center items-center py-6 cursor-pointer hover:bg-neutral-100 ${
+											active.key === tab ? "bg-neutral-100" : ""
+										}`}
+									>
+										{tab}
+									</li>
+								);
+							})}
+						</ul>
+					</Card>
 				</div>
-				<div className="flex-1">{active.controls}</div>
-			</Card>
-			<Card className="h-screen py-6 w-24 lg:w-36 shadow-none rounded-none">
-				<ul>
-					{Object.keys(tabs).map((tab, index) => {
-						return (
-							<li
-								onClick={() => onListClick(tab as activeTab)}
-								key={index}
-								className={`flex justify-center items-center py-6 cursor-pointer hover:bg-neutral-100 ${active.key === tab ? "bg-neutral-100" : ""}`}
-							>
-								{tab}
-							</li>
-						);
-					})}
-				</ul>
-			</Card>
-		</div>
+			) : (
+				<Card className="flex flex-col w-full h-full items-end shadow-none rounded-none p-6">
+					<div className="flex justify-end">
+						<Button size="icon" variant="outline" onClick={toggleSidebar}>
+							{component !== null ? <X /> : <ChevronLeft />}
+						</Button>
+					</div>
+					{component}
+				</Card>
+			)}
+		</>
 	);
 }
--- client/app/map/page.tsx
+++ client/app/map/page.tsx
@@ -4,27 +4,30 @@ import Map from "./_components/Map";
 import { SideBar } from "./_components/Sidebar";
 import { MapProvider } from "@/hooks/use-map";
 import { Toaster } from "@/components/ui/toaster";
+import { SidebarProvider } from "@/hooks/use-sidebar";
 
 export default function MapPage() {
 	return (
 		<MapProvider>
-			<main className="flex">
-				<div className="w-full lg:w-2/3 md:w-1/2 relative">
-					<div className="absolute w-screen flex justify-between p-2 z-50">
-						<div></div>
-						<Button size="icon" variant="outline" className="flex md:hidden">
-							<MenuIcon />
-						</Button>
+			<SidebarProvider>
+				<main className="flex relative">
+					<div className="flex-1 relative">
+						<div className="absolute w-screen flex justify-between p-2 z-50">
+							<div></div>
+							<Button size="icon" variant="outline" className="flex md:hidden">
+								<MenuIcon />
+							</Button>
+						</div>
+						<div className="flex items-center justify-center h-screen z-10 transition-transform transform">
+							<Map />
+						</div>
 					</div>
-					<div className="flex items-center justify-center h-screen z-10">
-						<Map />
+					<div className={`hidden md:block max-w-lg h-screen shadow-lg z-40`}>
+						<SideBar />
 					</div>
-				</div>
-				<div className="hidden md:block lg:w-1/3 md:w-1/2 h-screen">
-					<SideBar />
-				</div>
-			</main>
-			<Toaster />
+				</main>
+				<Toaster />
+			</SidebarProvider>
 		</MapProvider>
 	);
 }
