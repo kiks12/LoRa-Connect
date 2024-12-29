1d81eeb9f0582a0d8636d6b34ee498129c643292
Francis James Tolentino <francistolentino1107@gmail.com>
2024-12-30 00:21:39 +0800

    Add obstacle control working

--- client/app/map/_components/AdminControls.tsx
+++ client/app/map/_components/AdminControls.tsx
@@ -3,9 +3,33 @@ import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
 import { useMapContext } from "@/hooks/use-map";
 import EvacuationCenterListItem from "./EvacuationCenterListItem";
+import ObstacleListItem from "./ObstacleListItem";
+import { useSidebarContext } from "@/hooks/use-sidebar";
+import AddObstacleCard from "./AddObstacleCard";
 
 export default function AdminControls() {
-	const { monitorLocations, toggleMonitorLocations, evacuationCenters, showEvacuationCenters, toggleShowEvacuationCenters } = useMapContext();
+	const {
+		monitorLocations,
+		toggleMonitorLocations,
+		evacuationCenters,
+		showEvacuationCenters,
+		toggleShowEvacuationCenters,
+		obstacles,
+		showObstacles,
+		toggleShowObstacles,
+		toggleAddingObstacle,
+	} = useMapContext();
+	const { toggleSidebar, setComponent, setCloseComponentCallback: setCloseCopmonentCallback } = useSidebarContext();
+
+	function onAddObstacleClick() {
+		toggleAddingObstacle();
+		toggleSidebar();
+
+		setComponent(<AddObstacleCard />);
+		setCloseCopmonentCallback(() => () => {
+			toggleAddingObstacle();
+		});
+	}
 
 	return (
 		<div className="pt-6 pb-2 h-full flex flex-col justify-between">
@@ -15,6 +39,31 @@ export default function AdminControls() {
 						Monitor Bracelet Locations
 					</Label>
 					<Switch checked={monitorLocations} id="monitorLocations" onCheckedChange={toggleMonitorLocations} />
+				</div>
+				<div className="mt-2">
+					<h2 className="text-lg font-medium">Obstacles</h2>
+					<div className="flex justify-between items-center my-2 border rounded-md p-3">
+						<Label className="ml-3" htmlFor="showObstacles">
+							Show Obstacles
+						</Label>
+						<Switch checked={showObstacles} id="showObstacles" onCheckedChange={toggleShowObstacles} />
+					</div>
+					<div className="max-h-54 min-h-56 overflow-y-auto">
+						{obstacles.length === 0 ? (
+							<div className="text-center my-6">
+								<Label>No Obstacles</Label>
+							</div>
+						) : (
+							obstacles.map((obstacle, index) => {
+								return <ObstacleListItem obstacle={obstacle} key={index} />;
+							})
+						)}
+					</div>
+					<div>
+						<Button className="w-full mt-2" variant="secondary" onClick={onAddObstacleClick}>
+							Add Obstacle
+						</Button>
+					</div>
 				</div>
 			</div>
 			<div>
@@ -26,7 +75,7 @@ export default function AdminControls() {
 						</Label>
 						<Switch checked={showEvacuationCenters} id="showEvacuationCenters" onCheckedChange={toggleShowEvacuationCenters} />
 					</div>
-					<div className="max-h-96 min-h-fit overflow-y-auto">
+					<div className="max-h-54 min-h-56 overflow-y-auto">
 						{evacuationCenters.map((evacuationCenter, index) => {
 							return <EvacuationCenterListItem evacuationCenter={evacuationCenter} key={index} />;
 						})}
