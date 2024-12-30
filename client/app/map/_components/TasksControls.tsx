180e3c4640c36f71614b2a62c054822500b896db
Francis James Tolentino <francistolentino1107@gmail.com>
2024-12-30 22:19:39 +0800

    Initial Tasks Controls

--- client/app/map/_components/TasksControls.tsx
+++ client/app/map/_components/TasksControls.tsx
@@ -1,3 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
+
 export default function TasksControls() {
-	return <div>Tasks</div>;
+	return (
+		<div>
+			<Button className="w-full mt-2">Run Task Allocation</Button>
+			<div className="flex justify-between items-center my-2 border rounded-md p-3">
+				<Label className="ml-3" htmlFor="automaticTaskAllocation">
+					Automatically run task allocation
+				</Label>
+				<Switch checked={false} id="automaticTaskAllocation" onCheckedChange={() => {}} />
+			</div>
+		</div>
+	);
 }
