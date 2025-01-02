290aea6b256f0c33db5f51a7bc6fe74cd91e2881
Francis James Tolentino <francistolentino1107@gmail.com>
2025-01-02 22:05:09 +0800

    Added Status Indicator

--- client/app/map/_components/EvacuationCenterListItem.tsx
+++ client/app/map/_components/EvacuationCenterListItem.tsx
@@ -1,11 +1,17 @@
 import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { EvacuationCenters } from "@prisma/client";
+import { EvacuationCenterWithStatusIdentifier } from "@/types";
+import ShowStatusIndicator from "./ShowStatusIndicator";
 
-export default function EvacuationCenterListItem({ evacuationCenter }: { evacuationCenter: EvacuationCenters }) {
+export default function EvacuationCenterListItem({ evacuationCenter }: { evacuationCenter: EvacuationCenterWithStatusIdentifier }) {
 	return (
 		<Card className="my-1 shadow-none hover:shadow-sm cursor-pointer">
 			<CardHeader>
-				<CardTitle>{evacuationCenter.name}</CardTitle>
+				<div className="flex items-center">
+					<CardTitle className="hover:underline">{evacuationCenter.name}</CardTitle>
+					<div className="mx-2">
+						<ShowStatusIndicator show={evacuationCenter.showing} />
+					</div>
+				</div>
 				<CardDescription>Capacity: {evacuationCenter.capacity}</CardDescription>
 			</CardHeader>
 		</Card>
