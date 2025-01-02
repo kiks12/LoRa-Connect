dad365918848569587fff4d42b95fbbb6887eb70
Francis James Tolentino <francistolentino1107@gmail.com>
2025-01-02 16:06:46 +0800

    Added onClick functionality to obstacleListItem

--- client/app/map/_components/ObstacleListItem.tsx
+++ client/app/map/_components/ObstacleListItem.tsx
@@ -1,9 +1,9 @@
 import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Obstacle } from "@prisma/client";
 
-export default function ObstacleListItem({ obstacle }: { obstacle: Obstacle }) {
+export default function ObstacleListItem({ obstacle, onClick }: { obstacle: Obstacle; onClick: (obstacle: Obstacle) => void }) {
 	return (
-		<Card className="my-1 shadow-none hover:shadow-sm cursor-pointer">
+		<Card className="my-1 shadow-none hover:shadow-sm cursor-pointer" onClick={() => onClick(obstacle)}>
 			<CardHeader>
 				<CardTitle>{obstacle.name}</CardTitle>
 				<CardDescription>{obstacle.type}</CardDescription>
