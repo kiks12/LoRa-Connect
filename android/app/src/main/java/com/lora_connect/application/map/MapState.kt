8bbb565d5a84ded12d280e30938737f2025f8043
Francis James Tolentino <francistolentino1107@gmail.com>
2025-01-05 23:04:24 +0800

    Created MapViewModel and MapState

---
+++ android/app/src/main/java/com/lora_connect/application/map/MapState.kt
@@ -0,0 +1,6 @@
+package com.lora_connect.application.map
+
+data class MapState(
+    val latitude: Double = 15.0794,
+    val longitude: Double = 120.6200
+)
---
+++ android/app/src/main/java/com/lora_connect/application/map/MapViewModel.kt
@@ -0,0 +1,52 @@
+package com.lora_connect.application.map
+
+import android.annotation.SuppressLint
+import androidx.lifecycle.ViewModel
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.Priority
+import com.google.android.gms.tasks.CancellationTokenSource
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class MapViewModel(
+    private val fusedLocationProviderClient: FusedLocationProviderClient,
+    private val areLocationPermissionGranted: () -> Boolean,
+): ViewModel() {
+    private val _state = MutableStateFlow(MapState())
+    val state : StateFlow<MapState> = _state.asStateFlow()
+
+    fun setLatLng(latitude: Double, longitude: Double) {
+        _state.value = _state.value.copy(
+            latitude=latitude,
+            longitude=longitude
+        )
+    }
+
+    @SuppressLint("MissingPermission")
+    fun getCurrentLocation(
+        onGetCurrentLocationSuccess: (Pair<Double, Double>) -> Unit,
+        onGetCurrentLocationFailed: (Exception) -> Unit,
+        priority: Boolean = true
+    ) {
+        // Determine the accuracy priority based on the 'priority' parameter
+        val accuracy = if (priority) Priority.PRIORITY_HIGH_ACCURACY
+        else Priority.PRIORITY_BALANCED_POWER_ACCURACY
+
+        // Check if location permissions are granted
+        if (areLocationPermissionGranted()) {
+            // Retrieve the current location asynchronously
+            fusedLocationProviderClient.getCurrentLocation(
+                accuracy, CancellationTokenSource().token,
+            ).addOnSuccessListener { location ->
+                location?.let {
+                    // If location is not null, invoke the success callback with latitude and longitude
+                    onGetCurrentLocationSuccess(Pair(it.latitude, it.longitude))
+                }
+            }.addOnFailureListener { exception ->
+                // If an error occurs, invoke the failure callback with the exception
+                onGetCurrentLocationFailed(exception)
+            }
+        }
+    }
+}
