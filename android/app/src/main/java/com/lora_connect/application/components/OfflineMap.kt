58238a2bc6f1915746b434e6519c4f6788e04caa
Francis James Tolentino <francistolentino1107@gmail.com>
2025-01-05 23:04:09 +0800

    Adding Permission for Locations

--- android/app/src/main/java/com/lora_connect/application/components/OfflineMap.kt
+++ android/app/src/main/java/com/lora_connect/application/components/OfflineMap.kt
@@ -1,21 +1,30 @@
 package com.lora_connect.application.components
 
 import android.net.Uri
+import android.util.Log
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import com.lora_connect.application.MapStyleManager
+import com.lora_connect.application.map.MapViewModel
+import com.lora_connect.application.permissions.RequestLocationPermissionUsingRememberLauncherForActivityResult
 import org.maplibre.android.geometry.LatLng
 import org.maplibre.android.maps.Style
 import org.ramani.compose.CameraPosition
 import org.ramani.compose.MapLibre
 
 @Composable
-fun OfflineMap(modifier : Modifier = Modifier) {
-
+fun OfflineMap(
+    mapViewModel: MapViewModel,
+    modifier : Modifier = Modifier
+) {
     val context = LocalContext.current
+    val state by mapViewModel.state.collectAsState()
 
     val styleBuilder = remember {
         val styleManager = MapStyleManager(context)
@@ -33,10 +42,31 @@ fun OfflineMap(modifier : Modifier = Modifier) {
 
     val cameraPosition = rememberSaveable {
         CameraPosition(
-            target = LatLng(15.0794, 120.6200),
-            zoom = 7.0,
+            target = LatLng(state.latitude, state.longitude),
+            zoom = 9.0,
         )
     }
+
+    LaunchedEffect(state.latitude, state.longitude) {
+        cameraPosition.target = LatLng(latitude = state.latitude, longitude = state.longitude)
+    }
+
+    RequestLocationPermissionUsingRememberLauncherForActivityResult(
+        onPermissionGranted = {
+            mapViewModel.getCurrentLocation(
+                onGetCurrentLocationSuccess = {
+                    Log.w("OFFLINE MAP", it.toString())
+                    mapViewModel.setLatLng(it.first, it.second)
+                },
+                onGetCurrentLocationFailed = {
+                    it.localizedMessage?.let { it1 -> Log.w("OFFLINE MAP", it1) }
+                }
+            )
+        },
+        onPermissionDenied = {
+            Log.w("OFFLINE MAP", "PERMISSION DENIED")
+        }
+    )
 
     MapLibre(
         modifier = modifier,
