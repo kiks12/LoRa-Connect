6c78f00974e414641dd0e0969d390215a954f50b
Francis James Tolentino <francistolentino1107@gmail.com>
2025-01-07 13:28:03 +0800

    Created buildLocationComponentOptions and buildLocationComponentActivationOptions functions

--- android/app/src/main/java/com/lora_connect/application/MainActivity.kt
+++ android/app/src/main/java/com/lora_connect/application/MainActivity.kt
@@ -4,30 +4,36 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.Modifier
 import androidx.core.content.ContextCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-import com.lora_connect.application.components.OfflineMap
+import com.lora_connect.application.map.MapView
 import com.lora_connect.application.map.MapViewModel
 import com.lora_connect.application.ui.theme.ApplicationTheme
 import org.maplibre.android.MapLibre
+import org.maplibre.android.location.LocationComponentActivationOptions
+import org.maplibre.android.location.LocationComponentOptions
+import org.maplibre.android.location.engine.LocationEngineRequest
+import org.maplibre.android.maps.MapView
+import org.maplibre.android.maps.Style
 
 class MainActivity : ComponentActivity() {
-
+    private lateinit var mapView: MapView
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
         MapLibre.getInstance(this)
-        val mapViewModel = MapViewModel(fusedLocationProviderClient, ::areLocationPermissionsGranted)
+        mapView = MapView(this)
+        mapView.onCreate(savedInstanceState)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        val mapViewModel = MapViewModel(fusedLocationProviderClient, ::areLocationPermissionsGranted,::buildLocationComponentOptions, ::buildLocationComponentActivationOptions)
 
         setContent {
             ApplicationTheme {
-                OfflineMap(mapViewModel, modifier = Modifier.fillMaxSize())
+                MapView(mapView, mapViewModel)
             }
         }
     }
@@ -40,5 +46,53 @@ class MainActivity : ComponentActivity() {
 
         return fineLocationPermission == PackageManager.PERMISSION_GRANTED &&
                 coarseLocationPermission == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun buildLocationComponentOptions() : LocationComponentOptions {
+        return LocationComponentOptions.builder(this)
+            .pulseEnabled(true)
+            .build()
+    }
+
+    private fun buildLocationComponentActivationOptions(
+        style: Style,
+        locationComponentOptions: LocationComponentOptions
+    ): LocationComponentActivationOptions {
+        return LocationComponentActivationOptions
+            .builder(this, style)
+            .locationComponentOptions(locationComponentOptions)
+            .useDefaultLocationEngine(true)
+            .locationEngineRequest(
+                LocationEngineRequest.Builder(750)
+                    .setFastestInterval(750)
+                    .setPriority(LocationEngineRequest.PRIORITY_HIGH_ACCURACY)
+                    .build()
+            )
+            .build()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
     }
 }
