e5edb8b6ab71574f5ce355df2e42e15779cea855
Francis James Tolentino <francistolentino1107@gmail.com>
2025-01-07 13:49:13 +0800

    Added address to ownerSchema

--- client/schema/owners.ts
+++ client/schema/owners.ts
@@ -4,6 +4,9 @@ export const ownerSchema= z.object({
   name: z.string().min(1, {
     message: "Name should be at least 1 character"
   }),
+  address: z.string().nonempty({
+    message: "Address should not be empty"
+  }),
 	numberOfMembersInFamily: z.coerce.number().int().nonnegative({
     message: "Number should not be negative"
   }),
