aec6cab9c1fe8c4c1744c43c7ffd66f8cf325598
Francis James Tolentino <francistolentino1107@gmail.com>
2025-01-07 13:59:55 +0800

    Added Address to Owner DataTable Columns

--- client/app/(withHeader)/owners/columns.tsx
+++ client/app/(withHeader)/owners/columns.tsx
@@ -57,11 +57,11 @@ export const columns: ColumnDef<OwnerWithBracelet>[] = [
 		},
 	},
 	{
-		accessorKey: "braceletsBraceletId",
+		accessorKey: "address",
 		header: ({ column }) => {
 			return (
 				<Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
-					Bracelet ID
+					Address
 					<ArrowUpDown className="ml-2 h-4 w-4" />
 				</Button>
 			);
