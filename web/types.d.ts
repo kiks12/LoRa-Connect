92d4be63ca80bcbefa94c46a8c0100bd696bd238
Francis James Tolentino <francistolentino1107@gmail.com>
2025-01-08 18:20:06 +0800

    Created GraphHopperAPIResult type

--- web/types.d.ts
+++ web/types.d.ts
@@ -37,3 +37,41 @@ export type EvacuationCenterWithStatusIdentifier = EvacuationCenters & StatusIde
 export type OwnerWithStatusIdentifier = OwnerWithBracelet & StatusIdentifier
 
 export type RescuerWithStatusIdentifier = RescuerWithBracelet & StatusIdentifier
+
+export type GraphHopperAPIResult = {
+  hints: {
+    "visited_nodes.sum": number,
+    "visited_nodes.average": number,
+  },
+  info: {
+    copyrights: string[],
+    took: number,
+    road_data_timestamp: DateTime,
+  },
+  paths: {
+    distance: number,
+    weight: number,
+    time: number, 
+    transfers: number, 
+    points_encoded: boolean,
+    bbox: number[],
+    points: {
+      type: string,
+      coordinates: number[][]
+    },
+    instructions: {
+      distance: number,
+      heading: number, 
+      sign: number,
+      time: number,
+      interval: number[],
+      text: string,
+      street_name: string,
+    }[],
+    legs: [],
+    snapped_waypoints: {
+      type: string,
+      coordinates: number[][]
+    }
+  }[]
+}
