7c06c4045dfdb0b24f9f835d866414e48d35557a
Francis James Tolentino <francistolentino1107@gmail.com>
2025-01-08 18:19:51 +0800

    Added Rescuer Owner Route Colors

--- web/map-styles.ts
+++ web/map-styles.ts
@@ -1,3 +1,7 @@
 
 export const EVACUATION_CENTER_MARKER_COLOR = "#3a9e3e"
 export const OBSTACLE_MARKER_COLOR = "#FF0000"
+export const RESCUER_MARKER_COLOR = "#bf8900"
+export const OWNER_MARKER_COLOR = "#007cbf"
+
+export const ROUTE_COLOR = "#00a2e8"
