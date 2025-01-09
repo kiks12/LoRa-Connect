b1c03bce13d87f6ed190e09c1d07bf707dd556b6
Francis James Tolentino <francistolentino1107@gmail.com>
2025-01-09 12:05:33 +0800

    Operations CRUD Server functions

--- web/server/db/operations.ts
+++ web/server/db/operations.ts
@@ -1,5 +1,43 @@
 import { client } from "@/prisma/client"
+import { OPERATIONS_TAG } from "@/utils/tags"
+import { Operations } from "@prisma/client"
+import { unstable_cache } from "next/cache"
 
-export const getOperations = () => {
-  return client.operations.findMany() 
+export async function getLatestOperations() {
+  return await client.operations.findMany() 
+}
+
+export const getOperationsCached = unstable_cache(async () => {
+  return await client.operations.findMany()
+}, [OPERATIONS_TAG], {tags: [OPERATIONS_TAG]})
+
+export async function createOperation({operation}: {operation: Operations}) {
+  return await client.operations.create({
+    data: operation
+  })
+}
+
+export async function updateOperation({operation}: {operation: Operations}) {
+  return await client.operations.update({
+    where: {
+      missionId: operation.missionId
+    },
+    data: {
+      dateTime: operation.dateTime,
+      evacuationCentersEvacuationId: operation.evacuationCentersEvacuationId,
+      numberOfRescuee: operation.numberOfRescuee,
+      ownersOwnerId: operation.ownersOwnerId,
+      status: operation.status,
+      urgency: operation.urgency,
+      rescuersRescuerId: operation.rescuersRescuerId,
+    } 
+  })
+}
+
+export async function deleteOperation({operationId}: {operationId: number}) {
+  return await client.operations.delete({
+    where: {
+      missionId: operationId
+    }
+  }) 
 }
