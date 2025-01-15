2890516bd3007cdba5d8f1db0f9d810f82df988d
Francis James Tolentino <francistolentino1107@gmail.com>
2025-01-15 16:47:42 +0800

    Added path and marker states

--- android/app/src/main/java/com/lora_connect/application/map/MapState.kt
+++ android/app/src/main/java/com/lora_connect/application/map/MapState.kt
@@ -1,6 +1,11 @@
 package com.lora_connect.application.map
 
+import com.graphhopper.ResponsePath
+import org.maplibre.android.geometry.LatLng
+
 data class MapState(
     val latitude: Double = 15.0794,
-    val longitude: Double = 120.6200
+    val longitude: Double = 120.6200,
+    val markerLatLng: LatLng? = null,
+    val path: ResponsePath? = null
 )
