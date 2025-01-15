5960ce2bd4c390314d5747763b56ac25a73fb45c
Francis James Tolentino <francistolentino1107@gmail.com>
2025-01-15 16:46:29 +0800

    Graphhopper data

--- graphhopper/server.bash
+++ graphhopper/server.bash
@@ -1,1 +1,1 @@
-java -D"dw.graphhopper.datareader.file=philippines-latest.osm.pbf" -jar graphhopper*.jar server config-example.yml
+java -D"dw.graphhopper.datareader.file=philippines-latest-2.osm.pbf" -jar graphhopper-web-1.0.jar server config-example-v1.0.yml
