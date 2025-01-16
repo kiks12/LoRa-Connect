a0c2d3709123ad59088324d05bd7340a4eaf6cce
Francis James Tolentino <francistolentino1107@gmail.com>
2025-01-16 21:14:10 +0800

    Logging data

--- web/socket/actions/to-bracelets.ts
+++ web/socket/actions/to-bracelets.ts
@@ -1,11 +1,13 @@
+import { EvacuationInstruction } from "@/types"
 
 export function sendTransmitLocationSignalToBracelets(arg: string, callback: () => void) {
   console.log("SEND TRANSMIT LOCATION SIGNAL VIA LORA")
   // Process signal to transmit 
 }
 
-export function sendEvacuationInstructionToBracelets() {
+export function sendEvacuationInstructionToBracelets(arg: object) {
   console.log("SEND EVACUATION INSTRUCTION VIA LORA")
+  console.log(arg)
 }
 
 export function sendTaskToRescuer(arg: string, callback: () => void) {
