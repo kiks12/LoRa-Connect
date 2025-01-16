dde7f60c54d5d33fee5620b8f2ab688197b7ca94
Francis James Tolentino <francistolentino1107@gmail.com>
2025-01-16 13:50:00 +0800

    new config for graphhopper

--- graphhopper/server.bash
+++ graphhopper/server.bash
@@ -1,1 +1,7 @@
-java -D"dw.graphhopper.datareader.file=philippines-latest-2.osm.pbf" -jar graphhopper-web-1.0.jar server config-example-v1.0.yml
+
+# UNCOMMENT TO PRODUCE ROUTING DATA FOR MOBILE APP
+#java -D"dw.graphhopper.datareader.file=philippines-latest-2.osm.pbf" -jar graphhopper-web-1.0.jar server config-example-v1.0.yml
+
+# COMMENT WHEN PRODUCING ROUTING DATA FOR MOBILE APP
+java -D"dw.graphhopper.datareader.file=philippines-latest-2.osm.pbf" -jar graphhopper-web-10.0.jar server config-example.yml
+
