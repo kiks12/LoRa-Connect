4c8ee01f105f50a608cb36bba32ac5a906f619ee
Francis James Tolentino <francistolentino1107@gmail.com>
2025-01-20 21:08:56 +0800

    Implemented Error Handling on Rescuers API

--- web/app/api/rescuers/no-bracelets/route.ts
+++ web/app/api/rescuers/no-bracelets/route.ts
@@ -1,10 +1,36 @@
 import { getRescuersWithoutBracelets } from "@/server/db/rescuers";
+import { internalServerErrorReturnValue, methodNotAllowed, prismaClientInitializationErrorReturnValue, prismaClientValidationErrorReturnValue } from "@/utils/api";
+import { PrismaClientInitializationError, PrismaClientValidationError } from "@prisma/client/runtime/library";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const rescuers = await getRescuersWithoutBracelets()
+  try {
+    const rescuers = await getRescuersWithoutBracelets()
 
-  return NextResponse.json({
-    rescuers
-  })
+    return NextResponse.json({
+      rescuers
+    })
+  } catch (error) {
+    if (error instanceof PrismaClientInitializationError)
+      return prismaClientInitializationErrorReturnValue(error)
+    if (error instanceof PrismaClientValidationError)
+      return prismaClientValidationErrorReturnValue(error)
+    return internalServerErrorReturnValue(error)   
+  }
+}
+
+export async function POST() {
+  return methodNotAllowed({})
+}
+
+export async function PATCH() {
+  return methodNotAllowed({})
+}
+
+export async function PUT() {
+  return methodNotAllowed({})
+}
+
+export async function DELETE() {
+  return methodNotAllowed({})
 }
--- web/app/api/rescuers/route.ts
+++ web/app/api/rescuers/route.ts
@@ -1,9 +1,35 @@
 import { getRescuersLatest } from "@/server/db/rescuers";
+import { internalServerErrorReturnValue, methodNotAllowed, prismaClientInitializationErrorReturnValue, prismaClientValidationErrorReturnValue } from "@/utils/api";
+import { PrismaClientInitializationError, PrismaClientValidationError } from "@prisma/client/runtime/library";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const rescuers = await getRescuersLatest()
-  return NextResponse.json({
-    rescuers
-  })
+  try {
+    const rescuers = await getRescuersLatest()
+    return NextResponse.json({
+      rescuers
+    })
+  } catch (error) {
+    if (error instanceof PrismaClientInitializationError)
+      return prismaClientInitializationErrorReturnValue(error)
+    if (error instanceof PrismaClientValidationError)
+      return prismaClientValidationErrorReturnValue(error)
+    return internalServerErrorReturnValue(error)   
+  }
+}
+
+export async function POST() {
+  return methodNotAllowed({})
+}
+
+export async function PATCH() {
+  return methodNotAllowed({})
+}
+
+export async function PUT() {
+  return methodNotAllowed({})
+}
+
+export async function DELETE() {
+  return methodNotAllowed({})
 }
