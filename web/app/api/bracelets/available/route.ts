df266c8cc8a056d6d02c9fb7df6eedcd55cd3267
Francis James Tolentino <francistolentino1107@gmail.com>
2025-01-20 21:10:10 +0800

    Implemented Error Handling in Bracelets API

--- web/app/api/bracelets/available/route.ts
+++ web/app/api/bracelets/available/route.ts
@@ -1,10 +1,36 @@
 import { getAvailableBracelets } from "@/server/db/bracelets";
+import { internalServerErrorReturnValue, methodNotAllowed, prismaClientInitializationErrorReturnValue, prismaClientValidationErrorReturnValue } from "@/utils/api";
+import { PrismaClientInitializationError, PrismaClientValidationError } from "@prisma/client/runtime/library";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const bracelets = await getAvailableBracelets()
+  try {
+    const bracelets = await getAvailableBracelets()
+    
+    return NextResponse.json({
+      bracelets 
+    })
+  } catch (error) {
+    if (error instanceof PrismaClientInitializationError)
+      return prismaClientInitializationErrorReturnValue(error)
+    if (error instanceof PrismaClientValidationError)
+      return prismaClientValidationErrorReturnValue(error)
+    return internalServerErrorReturnValue(error) 
+  }
+}
 
-  return NextResponse.json({
-    bracelets 
-  })
+export async function POST() {
+  return methodNotAllowed({})
+}
+
+export async function DELETE() {
+  return methodNotAllowed({})
+}
+
+export async function PUT() {
+  return methodNotAllowed({})
+}
+
+export async function PATCH() {
+  return methodNotAllowed({})
 }
--- web/app/api/bracelets/update-location/route.ts
+++ web/app/api/bracelets/update-location/route.ts
@@ -1,16 +1,56 @@
 import { updateBraceletLocation } from "@/server/db/bracelets"
+import { internalServerErrorReturnValue, methodNotAllowed, prismaClientInitializationErrorReturnValue, prismaClientValidationErrorReturnValue, syntaxErrorReturnValue, typeErrorReturnValue } from "@/utils/api"
+import { PrismaClientInitializationError, PrismaClientValidationError } from "@prisma/client/runtime/library"
 import { NextResponse } from "next/server"
 
+/*
 
+SAMPLE BODY
+{
+  braceletId: string,
+  latitude: number,
+  longitude: number
+}
+
+*/
 export async function PATCH(req: Request) {
-  const {braceletId, latitude, longitude}: {
-    braceletId: string, 
-    latitude: number, 
-    longitude: number
-  }= await req.json()
-  await updateBraceletLocation({braceletId, latitude: parseFloat(latitude.toFixed(6)), longitude: parseFloat(longitude.toFixed(6))})
+  try {
+    const {braceletId, latitude, longitude}: {
+      braceletId: string, 
+      latitude: number, 
+      longitude: number
+    }= await req.json()
+    await updateBraceletLocation({braceletId, latitude: parseFloat(latitude.toFixed(6)), longitude: parseFloat(longitude.toFixed(6))})
 
-  return NextResponse.json({
-    message: "Update Bracelet Location"
-  })
+    return NextResponse.json({
+      message: "Update Bracelet Location"
+    })
+  } catch (error) {
+    console.error(error)
+    if (error instanceof SyntaxError)
+      return syntaxErrorReturnValue(error)
+    if (error instanceof TypeError)
+      return typeErrorReturnValue(error)
+    if (error instanceof PrismaClientInitializationError)
+      return prismaClientInitializationErrorReturnValue(error)
+    if (error instanceof PrismaClientValidationError)
+      return prismaClientValidationErrorReturnValue(error)
+    return internalServerErrorReturnValue(error)
+  }
+}
+
+export async function POST() {
+  return methodNotAllowed({})
+}
+
+export async function DELETE() {
+  return methodNotAllowed({})
+}
+
+export async function PUT() {
+  return methodNotAllowed({})
+}
+
+export async function GET() {
+  return methodNotAllowed({})
 }
