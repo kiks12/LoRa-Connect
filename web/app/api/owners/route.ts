348ad5a9eb027d761fa7d59bf03659bfe699dec5
Francis James Tolentino <francistolentino1107@gmail.com>
2025-01-20 21:09:09 +0800

    Implemented Error Handling on Owners API

--- web/app/api/owners/route.ts
+++ web/app/api/owners/route.ts
@@ -1,9 +1,35 @@
 import { getOwnersLatest } from "@/server/db/owners";
+import { internalServerErrorReturnValue, methodNotAllowed, prismaClientInitializationErrorReturnValue, prismaClientValidationErrorReturnValue } from "@/utils/api";
+import { PrismaClientInitializationError, PrismaClientValidationError } from "@prisma/client/runtime/library";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const owners = await getOwnersLatest()
-  return NextResponse.json({
-    owners
-  })
+  try {
+    const owners = await getOwnersLatest()
+    return NextResponse.json({
+      owners
+    })
+  } catch (error) {
+    if (error instanceof PrismaClientInitializationError)
+      return prismaClientInitializationErrorReturnValue(error)
+    if (error instanceof PrismaClientValidationError)
+      return prismaClientValidationErrorReturnValue(error)
+    return internalServerErrorReturnValue(error)   
+  }
+}
+
+export async function POST() {
+  return methodNotAllowed({})
+}
+
+export async function DELETE() {
+  return methodNotAllowed({})
+}
+
+export async function PUT() {
+  return methodNotAllowed({})
+}
+
+export async function PATCH() {
+  return methodNotAllowed({})
 }
