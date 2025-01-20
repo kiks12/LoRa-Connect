0002a773c7454c22cb44ad86f193e132fa940acf
Francis James Tolentino <francistolentino1107@gmail.com>
2025-01-20 21:08:38 +0800

    Created utility functions for generalized error handling

--- web/utils/api.ts
+++ web/utils/api.ts
@@ -1,3 +1,4 @@
+import { PrismaClientInitializationError, PrismaClientValidationError } from "@prisma/client/runtime/library";
 import { NextResponse } from "next/server";
 
 type ApiMethods = "GET" | "POST" | "DELETE" | "PUT" | "PATCH"
@@ -8,5 +9,61 @@ export function methodNotAllowed({allowedMethods = []}: {allowedMethods?: ApiMet
     allowedMethods: allowedMethods.length > 0 ? allowedMethods : null
   }, {
     status: 405
+  })
+}
+
+export function prismaClientInitializationErrorReturnValue(error: PrismaClientInitializationError) {
+  return NextResponse.json({
+    message: error.message,
+    name: error.name,
+    cause: error.cause,
+    stack: error.stack,
+    clientVersion: error.clientVersion,
+    retryable: error.retryable,
+    errorCode: error.errorCode
+  }, {
+    status: 503
+  })
+}
+
+export function prismaClientValidationErrorReturnValue(error: PrismaClientValidationError) {
+  return NextResponse.json({
+    message: error.message,
+    name: error.name,
+    cause: error.cause,
+    stack: error.stack,
+    clientVersion: error.clientVersion,
+  }, {
+    status: 400
+  })
+}
+
+export function syntaxErrorReturnValue(error: SyntaxError) {
+  return NextResponse.json({
+    message: error.message,
+    name: error.name,
+    cause: error.cause,
+    stack: error.stack,
+  }, {
+    status: 400 
+  })
+}
+
+export function typeErrorReturnValue(error: TypeError) {
+  return NextResponse.json({
+    message: error.message,
+    name: error.name,
+    cause: error.cause,
+    stack: error.stack,
+  }, {
+    status: 400 
+  })
+}
+
+export function internalServerErrorReturnValue(error: unknown) {
+  return NextResponse.json({
+    error
+  }, {
+    status: 500
   })
 }
