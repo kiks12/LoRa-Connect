6d738064a30c31885e785d8a2ca58e20ff24ac4b
Francis James Tolentino <francistolentino1107@gmail.com>
2025-01-20 21:10:25 +0800

    Implemented error handling in Evacuation Centers API

--- web/app/api/evacuation-centers/route.ts
+++ web/app/api/evacuation-centers/route.ts
@@ -1,10 +1,36 @@
 import { getLatestEvacuationCenters } from "@/server/db/evacuationCenters";
+import { internalServerErrorReturnValue, methodNotAllowed, prismaClientInitializationErrorReturnValue, prismaClientValidationErrorReturnValue } from "@/utils/api";
+import { PrismaClientInitializationError, PrismaClientValidationError } from "@prisma/client/runtime/library";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const evacuationCenters = await getLatestEvacuationCenters();
+  try {
+    const evacuationCenters = await getLatestEvacuationCenters();
 
-  return NextResponse.json({
-    evacuationCenters
-  })
+    return NextResponse.json({
+      evacuationCenters
+    })
+  } catch (error) {
+    if (error instanceof PrismaClientInitializationError)
+      return prismaClientInitializationErrorReturnValue(error)
+    if (error instanceof PrismaClientValidationError) 
+      return prismaClientValidationErrorReturnValue(error)
+    return internalServerErrorReturnValue(error)
+  }
+}
+
+export async function POST() {
+  return methodNotAllowed({})
+}
+
+export async function DELETE() {
+  return methodNotAllowed({})
+}
+
+export async function PUT() {
+  return methodNotAllowed({})
+}
+
+export async function PATCH() {
+  return methodNotAllowed({})
 }
