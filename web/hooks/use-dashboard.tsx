8c9945b36ffb7f132ad7736cd05e4f319417c33a
Francis James Tolentino <francistolentino1107@gmail.com>
2025-01-24 23:51:20 +0800

    Working dashboard functionality -- need to refactor

--- web/hooks/use-dashboard.tsx
+++ web/hooks/use-dashboard.tsx
@@ -4,6 +4,12 @@ import { OperationsWithPayload, OwnerWithBracelet, RescuerWithBracelet } from "@
 import { EvacuationCenters, Obstacle } from "@prisma/client";
 import { useEffect, useMemo, useState } from "react";
 
+const DAYS_MAP = {
+	"Last 7 Days": 7,
+	"Last 30 Days": 30,
+	"Last 60 Days": 60,
+};
+
 export default function useDashboard() {
 	const [owners, setOwners] = useState<{ owners: OwnerWithBracelet[]; loading: boolean }>({
 		owners: [],
@@ -37,10 +43,53 @@ export default function useDashboard() {
 		obstacles: [],
 		loading: true,
 	});
+	const [operationsLineChartData, setOperationsLineChartData] = useState<{
+		data: { date: Date; count: number }[];
+		loading: boolean;
+		option: "Last 7 Days" | "Last 30 Days" | "Last 60 Days";
+	}>({
+		data: [],
+		loading: true,
+		option: "Last 7 Days",
+	});
 	const [operations, setOperations] = useState<{ operations: OperationsWithPayload[]; loading: boolean }>({
 		operations: [],
 		loading: true,
 	});
+	const operationsBreakdown = useMemo(() => {
+		const breakdown: {
+			assigned: OperationsWithPayload[];
+			pending: OperationsWithPayload[];
+			canceled: OperationsWithPayload[];
+			complete: OperationsWithPayload[];
+			failed: OperationsWithPayload[];
+			low: OperationsWithPayload[];
+			moderate: OperationsWithPayload[];
+			severe: OperationsWithPayload[];
+		} = {
+			assigned: [],
+			pending: [],
+			canceled: [],
+			complete: [],
+			failed: [],
+			low: [],
+			moderate: [],
+			severe: [],
+		};
+
+		operations.operations.forEach((operation) => {
+			if (operation.status === "ASSIGNED") breakdown.assigned.push(operation);
+			if (operation.status === "PENDING") breakdown.pending.push(operation);
+			if (operation.status === "CANCELED") breakdown.canceled.push(operation);
+			if (operation.status === "COMPLETE") breakdown.complete.push(operation);
+			if (operation.status === "FAILED") breakdown.failed.push(operation);
+			if (operation.urgency === "LOW") breakdown.low.push(operation);
+			if (operation.urgency === "MODERATE") breakdown.moderate.push(operation);
+			if (operation.urgency === "SEVERE") breakdown.severe.push(operation);
+		});
+
+		return breakdown;
+	}, [operations.operations]);
 
 	useEffect(() => {
 		setOwners((prev) => ({ ...prev, loading: true }));
@@ -70,5 +119,34 @@ export default function useDashboard() {
 			.then(({ obstacles }) => setObstacles(() => ({ obstacles, loading: false })));
 	}, []);
 
-	return { owners, ownersDoughnut, rescuers, rescuersDoughnut, evacuationCenters, obstacles };
+	useEffect(() => {
+		setOperations((prev) => ({ ...prev, loading: true }));
+		fetch("/api/operations")
+			.then((res) => res.json())
+			.then(({ operations }) => setOperations(() => ({ operations, loading: false })));
+	}, []);
+
+	useEffect(() => {
+		setOperationsLineChartData((prev) => ({ ...prev, loading: true }));
+		fetch(`/api/operations/last-days?lastDays=${DAYS_MAP[operationsLineChartData.option]}`)
+			.then((res) => res.json())
+			.then(({ operations }) => setOperationsLineChartData((prev) => ({ ...prev, data: operations, loading: false })));
+	}, [operationsLineChartData.option]);
+
+	function onOperationsLineChartOptionChange(option: "Last 7 Days" | "Last 30 Days" | "Last 60 Days") {
+		setOperationsLineChartData((prev) => ({ ...prev, option }));
+	}
+
+	return {
+		owners,
+		ownersDoughnut,
+		rescuers,
+		rescuersDoughnut,
+		evacuationCenters,
+		obstacles,
+		operations,
+		operationsBreakdown,
+		operationsLineChartData,
+		onOperationsLineChartOptionChange,
+	};
 }
