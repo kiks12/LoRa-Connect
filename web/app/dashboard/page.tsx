f5362a76abce602da10495a4f40c640d8bcd0017
Francis James Tolentino <francistolentino1107@gmail.com>
2025-01-25 23:47:56 +0800

    UI Revisions

--- web/app/dashboard/page.tsx
+++ web/app/dashboard/page.tsx
@@ -2,17 +2,14 @@
 
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
-import { ChevronDown, ChevronLeft } from "lucide-react";
+import { Ban, ChevronLeft, Hospital, RefreshCcw } from "lucide-react";
 import Link from "next/link";
-import TriDataSection from "./_components/TriDataSection";
 import DataSection from "./_components/DataSection";
 import useDashboard from "@/hooks/use-dashboard";
-import DoughnutBreakdown from "./_components/DoughnutBreakdown";
 import { Chart as ChartJs, ArcElement, Tooltip, Legend, CategoryScale, LinearScale, PointElement, LineElement, Title } from "chart.js";
-import { Line } from "react-chartjs-2";
-import { Card, CardContent } from "@/components/ui/card";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
-import Spinner from "../components/Spinner";
+import LineChartWithDropdown from "./_components/LineChartWithDropdown";
+import OwnersData from "./_components/OwnersData";
+import RescuersData from "./_components/RescuersData";
 
 ChartJs.register(CategoryScale, LinearScale, PointElement, LineElement, Title, ArcElement, Tooltip, Legend);
 
@@ -28,86 +25,70 @@ export default function DashboardPage() {
 		operations,
 		operationsLineChartData,
 		onOperationsLineChartOptionChange,
+		refreshDashboard,
 	} = useDashboard();
 
 	return (
 		<main className="min-w-screen min-h-screen bg-neutral-50">
 			<div className="max-w-7xl mx-auto pt-10">
-				<div className="flex items-center">
-					<div className="mr-4">
-						<Link href="/">
-							<Button variant="outline" size="icon">
-								<ChevronLeft />
-							</Button>
-						</Link>
+				<div className="flex items-center justify-between">
+					<div className="flex items-center">
+						<div className="mr-4">
+							<Link href="/">
+								<Button variant="outline" size="icon">
+									<ChevronLeft />
+								</Button>
+							</Link>
+						</div>
+						<div>
+							<h1 className="text-2xl font-semibold">LoRa-Connect Dashboard</h1>
+							<Label>{new Date().toDateString()}</Label>
+						</div>
 					</div>
-					<div>
-						<h1 className="text-2xl font-semibold">LoRa-Connect Dashboard</h1>
-						<Label>{new Date().toDateString()}</Label>
+					<div className="flex">
+						<Button variant="outline" className="mr-2" onClick={refreshDashboard}>
+							<RefreshCcw />
+							Refresh
+						</Button>
+						<Button>Generate Report</Button>
 					</div>
 				</div>
 				<div className="flex flex-col lg:flex-row mt-4">
-					<div className="flex-1 mr-1">
-						<div>
-							<Link href="/owners">
-								<h2 className="text-xl font-medium hover:underline">Owners</h2>
-							</Link>
-						</div>
-						<div className="flex flex-col md:flex-row mt-2">
-							<div className="flex-1 mr-1">
-								<TriDataSection
-									loading={owners.loading}
-									firstTitle="Total Owners"
-									firstData={owners.owners.length}
-									secondTitle="Owners w/ Bracelets"
-									secondData={owners.owners.filter((o) => o.bracelet).length}
-									thirdTitle="Owners w/o Bracelets"
-									thirdData={owners.owners.filter((o) => !o.bracelet).length}
-								/>
-							</div>
-							<div className="ml-1">
-								<DoughnutBreakdown data={ownersDoughnut.data} labels={ownersDoughnut.labels} />
-							</div>
-						</div>
+					<div className="flex-1 mr-2">
+						<OwnersData data={owners} doughnutData={ownersDoughnut} />
 					</div>
-					<div className="flex-1 ml-1">
-						<div>
-							<Link href="/rescuers">
-								<h2 className="text-xl font-medium hover:underline">Rescuers</h2>
-							</Link>
-						</div>
-						<div className="flex flex-col md:flex-row mt-2">
-							<div className="mr-1">
-								<DoughnutBreakdown data={rescuersDoughnut.data} labels={rescuersDoughnut.labels} />
-							</div>
-							<div className="flex-1 ml-1">
-								<TriDataSection
-									loading={rescuers.loading}
-									firstTitle="Total Rescuers"
-									firstData={rescuers.rescuers.length}
-									secondTitle="Rescuers w/ Bracelets"
-									secondData={rescuers.rescuers.filter((r) => r.bracelet).length}
-									thirdTitle="Rescuers w/o Bracelets"
-									thirdData={rescuers.rescuers.filter((r) => !r.bracelet).length}
-								/>
-							</div>
-						</div>
+					<div className="flex-1 ml-2">
+						<RescuersData data={rescuers} doughnutData={rescuersDoughnut} />
 					</div>
 				</div>
-				<div className="mt-4 flex">
-					<div className="flex-1 mr-1">
+				<div className="mt-8 flex">
+					<div className="flex-1 mr-2">
 						<DataSection
+							icon={
+								<div className="p-4 rounded-full bg-blue-100 border border-blue-300 ">
+									<Hospital className="text-blue-700" />
+								</div>
+							}
 							link="/evacuationCenters"
 							loading={evacuationCenters.loading}
 							title="Total Evacuation Centers"
 							data={evacuationCenters.evacuationCenters.length}
 						/>
 					</div>
-					<div className="flex-1 ml-1">
-						<DataSection loading={obstacles.loading} title="Total Obstacles" data={obstacles.obstacles.length} />
+					<div className="flex-1 ml-2">
+						<DataSection
+							loading={obstacles.loading}
+							title="Total Obstacles"
+							data={obstacles.obstacles.length}
+							icon={
+								<div className="p-4 rounded-full bg-red-100 border border-red-300 ">
+									<Ban className="text-red-700" />
+								</div>
+							}
+						/>
 					</div>
 				</div>
-				<div className="mt-4">
+				<div className="mt-8">
 					<div>
 						<Link href="/missions">
 							<h2 className="text-xl font-medium hover:underline">Missions</h2>
@@ -124,56 +105,19 @@ export default function DashboardPage() {
 							<DataSection loading={operations.loading} title="SEVERE URGENCY" data={operationsBreakdown.severe.length} />
 						</div>
 					</div>
-					<Card className="mt-2 shadow-md border-0">
-						<CardContent className="p-4">
-							<div>
-								<DropdownMenu>
-									<DropdownMenuTrigger>
-										<Button variant="outline">
-											{operationsLineChartData.option} <ChevronDown />
-										</Button>
-									</DropdownMenuTrigger>
-									<DropdownMenuContent>
-										<DropdownMenuItem onClick={() => onOperationsLineChartOptionChange("Last 7 Days")}>Last 7 Days</DropdownMenuItem>
-										<DropdownMenuItem onClick={() => onOperationsLineChartOptionChange("Last 30 Days")}>Last 30 Days</DropdownMenuItem>
-										<DropdownMenuItem onClick={() => onOperationsLineChartOptionChange("Last 60 Days")}>Last 60 Days</DropdownMenuItem>
-									</DropdownMenuContent>
-								</DropdownMenu>
-							</div>
-							<div>
-								{operationsLineChartData.loading ? (
-									<div className="flex items-center justify-center">
-										<Spinner />
-									</div>
-								) : (
-									<Line
-										options={{
-											responsive: true,
-											plugins: {
-												legend: {
-													position: "top" as const,
-												},
-											},
-										}}
-										data={{
-											labels: operationsLineChartData.data.map((o) => new Date(o.date).toLocaleDateString()),
-											datasets: [
-												{
-													label: "Operations",
-													data: operationsLineChartData.data.map((o) => o.count),
-													fill: true,
-													borderColor: "rgba(75,192,192,1)", // Line color
-													backgroundColor: "rgba(75,192,192,0.5)",
-													tension: 0.1, // Smooth curves
-												},
-											],
-										}}
-									/>
-								)}
-							</div>
-						</CardContent>
-					</Card>
-
+					<div>
+						<LineChartWithDropdown
+							labels={operationsLineChartData.data.map((o) => new Date(o.date).toLocaleDateString())}
+							data={operationsLineChartData.data.map((o) => o.count)}
+							option={operationsLineChartData.option}
+							loading={operationsLineChartData.loading}
+							options={[
+								{ value: "Last 7 Days", onClick: () => onOperationsLineChartOptionChange("Last 7 Days") },
+								{ value: "Last 30 Days", onClick: () => onOperationsLineChartOptionChange("Last 30 Days") },
+								{ value: "Last 60 Days", onClick: () => onOperationsLineChartOptionChange("Last 60 Days") },
+							]}
+						/>
+					</div>
 					<div className="flex mt-2 mb-10">
 						<div className="flex-1 mr-1">
 							<DataSection loading={operations.loading} title="ASSIGNED" data={operationsBreakdown.assigned.length} />
