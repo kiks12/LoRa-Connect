af55259c561d6d6a3e29bf0363099d9edc7b9799
Francis James Tolentino <francistolentino1107@gmail.com>
2025-01-27 21:10:19 +0800

    Implemented GET Bracelets API

---
+++ web/app/api/bracelets/route.ts
@@ -0,0 +1,37 @@
+
+import { getLatestBracelets } from "@/server/db/bracelets";
+import { internalServerErrorReturnValue, methodNotAllowed, prismaClientInitializationErrorReturnValue, prismaClientValidationErrorReturnValue } from "@/utils/api";
+import { PrismaClientInitializationError, PrismaClientValidationError } from "@prisma/client/runtime/library";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const bracelets = await getLatestBracelets()
+    
+    return NextResponse.json({
+      bracelets 
+    })
+  } catch (error) {
+    if (error instanceof PrismaClientInitializationError)
+      return prismaClientInitializationErrorReturnValue(error)
+    if (error instanceof PrismaClientValidationError)
+      return prismaClientValidationErrorReturnValue(error)
+    return internalServerErrorReturnValue(error) 
+  }
+}
+
+export async function POST() {
+  return methodNotAllowed({})
+}
+
+export async function DELETE() {
+  return methodNotAllowed({})
+}
+
+export async function PUT() {
+  return methodNotAllowed({})
+}
+
+export async function PATCH() {
+  return methodNotAllowed({})
+}
