300f20dbc26d06cdd02ec6d8b352e1e1800d2155
Francis James Tolentino <francistolentino1107@gmail.com>
2025-01-27 23:05:03 +0800

    Refactored UI

--- web/app/dashboard/_components/LineChartWithDropdown.tsx
+++ web/app/dashboard/_components/LineChartWithDropdown.tsx
@@ -1,6 +1,4 @@
 import Spinner from "@/app/components/Spinner";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { ChevronDown } from "lucide-react";
 import { Line } from "react-chartjs-2";
@@ -20,61 +18,62 @@ export default function LineChartWithDropdown({
 }) {
 	return (
 		<div>
-			<Card className="mt-2 shadow-md border-0">
-				<CardContent className="p-4">
-					<div>
-						<DropdownMenu>
-							<DropdownMenuTrigger asChild>
-								<div>
-									<Button variant="outline">
-										{option} <ChevronDown />
-									</Button>
-								</div>
-							</DropdownMenuTrigger>
-							<DropdownMenuContent>
-								{options.map((o, index) => {
-									return (
-										<DropdownMenuItem onClick={o.onClick} key={index}>
-											{o.value}
-										</DropdownMenuItem>
-									);
-								})}
-							</DropdownMenuContent>
-						</DropdownMenu>
-					</div>
-					<div>
-						{loading ? (
-							<div className="flex items-center justify-center">
-								<Spinner />
+			<div className="flex items-center justify-between">
+				<div>
+					<h2 className="text-lg font-semibold">Missions Trends</h2>
+				</div>
+				<div>
+					<DropdownMenu>
+						<DropdownMenuTrigger>
+							<div className="flex items-center p-2 px-4 border shadow-sm rounded-lg text-sm">
+								{option} <ChevronDown className="ml-2" />
 							</div>
-						) : (
-							<Line
-								options={{
-									responsive: true,
-									plugins: {
-										legend: {
-											position: "top" as const,
-										},
-									},
-								}}
-								data={{
-									labels: labels,
-									datasets: [
-										{
-											label: "Operations",
-											data: data,
-											fill: true,
-											borderColor: "rgba(75,192,192,1)", // Line color
-											backgroundColor: "rgba(75,192,192,0.5)",
-											tension: 0.1, // Smooth curves
-										},
-									],
-								}}
-							/>
-						)}
+						</DropdownMenuTrigger>
+						<DropdownMenuContent>
+							{options.map((o, index) => {
+								return (
+									<DropdownMenuItem onClick={o.onClick} key={index}>
+										{o.value}
+									</DropdownMenuItem>
+								);
+							})}
+						</DropdownMenuContent>
+					</DropdownMenu>
+				</div>
+			</div>
+			<div className="flex items-center justify-center">
+				{loading ? (
+					<div className="flex items-center justify-center">
+						<Spinner />
 					</div>
-				</CardContent>
-			</Card>
+				) : (
+					<Line
+						className="w-full"
+						height={80}
+						options={{
+							responsive: true,
+							plugins: {
+								legend: {
+									position: "top" as const,
+								},
+							},
+						}}
+						data={{
+							labels: labels,
+							datasets: [
+								{
+									label: "Operations",
+									data: data,
+									fill: true,
+									borderColor: "rgba(75,192,192,1)", // Line color
+									backgroundColor: "rgba(75,192,192,0.5)",
+									tension: 0.5, // Smooth curves
+								},
+							],
+						}}
+					/>
+				)}
+			</div>
 		</div>
 	);
 }
