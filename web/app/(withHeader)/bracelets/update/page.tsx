e0fa6ccacf369b54f9f00e53e7df576cdfa4bc09
Francis James Tolentino <francistolentino1107@gmail.com>
2025-01-27 19:09:12 +0800

    Getting type from url and passing it to BraceletForm

--- web/app/(withHeader)/bracelets/update/page.tsx
+++ web/app/(withHeader)/bracelets/update/page.tsx
@@ -6,12 +6,13 @@ import { BraceletForm } from "../_components/BraceletForm";
 export default async function UpdateBraceletPage({ searchParams }: { searchParams: { [key: string]: string | undefined } }) {
 	const braceletId: string = searchParams.braceletId ?? "";
 	const name: string = searchParams.name ?? "";
+	const type: string = searchParams.type ?? "";
 
 	return (
 		<main>
 			<ContainerWithTitleAndBackButton title="Update Bracelet" previousLink="/bracelets">
 				<div className="mt-4 p-16">
-					<BraceletForm type="UPDATE" braceletId={braceletId} name={name} />
+					<BraceletForm formType="UPDATE" braceletId={braceletId} name={name} type={type} />
 				</div>
 			</ContainerWithTitleAndBackButton>
 		</main>
