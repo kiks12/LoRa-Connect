9820a6010ca4ef69fc11945f2acc189418f47d86
Francis James Tolentino <francistolentino1107@gmail.com>
2025-01-28 22:10:41 +0800

    Changed Primary, Secondary, Tertiary Colors

--- android/app/src/main/java/com/lora_connect/application/ui/theme/Color.kt
+++ android/app/src/main/java/com/lora_connect/application/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.lora_connect.application.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Purple80 = Color(0xFF000000)
+val PurpleGrey80 = Color(0xFF3C3C3C)
+val Pink80 = Color(0xFFE2E2E2)
 
-val Purple40 = Color(0xFF6650a4)
+val Purple40 = Color(0xFF5C5C5C)
 val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Pink40 = Color(0xFFEEEEEE)
