bb6940afac7ab80701f49b3d9b24ed07a07d7756
Francis James Tolentino <francistolentino1107@gmail.com>
2025-01-29 21:43:19 +0800

    Created TaskRepository for handling functions from TaskDao

---
+++ android/app/src/main/java/com/lora_connect/application/repositories/TaskRepository.kt
@@ -0,0 +1,30 @@
+package com.lora_connect.application.repositories
+
+import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
+import com.lora_connect.application.room.AppDatabase
+import com.lora_connect.application.room.entities.Task
+import java.util.Date
+
+class TaskRepository(context: Context){
+    private val db = AppDatabase.getDatabase(context)
+    private val taskDao = db.taskDao()
+
+    suspend fun createTask(task: Task) {
+        taskDao.insertAll(task)
+    }
+
+    suspend fun getAllTasks() : List<Task> {
+        return taskDao.getTasks()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    suspend fun getTasksToday() : List<Task> {
+        return taskDao.getTasksToday(Date())
+    }
+
+    suspend fun updateTask(task: Task) {
+        taskDao.updateTask(task)
+    }
+}
