de9e96dbbb1d3df1ad11c6eba59c1a18ba9fc4d9
Francis James Tolentino <francistolentino1107@gmail.com>
2025-02-01 20:48:34 +0800

    Added few UI elements and created function startBluetoothDataService

--- android/app/src/main/java/com/lora_connect/application/map/MapActivity.kt
+++ android/app/src/main/java/com/lora_connect/application/map/MapActivity.kt
@@ -1,12 +1,28 @@
 package com.lora_connect.application.map
 
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.ui.Modifier
 import androidx.core.content.ContextCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import com.lora_connect.application.services.BluetoothDataService
+import com.lora_connect.application.tasks.list.TaskListActivity
 import com.lora_connect.application.ui.theme.ApplicationTheme
 import com.lora_connect.application.utils.copyAssetsToFilesDir
 import kotlinx.coroutines.CoroutineScope
@@ -25,6 +41,7 @@ class MapActivity : ComponentActivity() {
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private val ioScope = CoroutineScope(Dispatchers.IO)
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -36,6 +53,7 @@ class MapActivity : ComponentActivity() {
         val offlineRouting = OfflineRouting(this)
         offlineRouting.initializeGraphHopper()
         val mapViewModel = MapViewModel(
+            application,
             fusedLocationProviderClient,
             ::areLocationPermissionsGranted,
             ::buildLocationComponentOptions,
@@ -45,9 +63,46 @@ class MapActivity : ComponentActivity() {
 
         setContent {
             ApplicationTheme {
-                MapView(mapView, mapViewModel)
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = {  },
+                            navigationIcon = { 
+                                IconButton(onClick = { /*TODO*/ }) {
+                                    Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Go Back")
+                                }
+                            },
+                            actions = {
+                                TextButton(onClick = ::startTasksList) {
+                                    Text(text = "Tasks")
+                                }
+                            }
+                        )
+                    }
+                ){ innerPadding ->
+                    Box(modifier = Modifier.padding(innerPadding)) {
+                        MapView(mapView, mapViewModel)
+                    }
+                }
             }
         }
+
+        // ONLY UNCOMMENT IF AUTHENTICATION IS RUNNING
+        // startBluetoothDataService()
+    }
+
+    private fun startBluetoothDataService() {
+        val intent = Intent(this, BluetoothDataService::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(intent)
+        } else {
+            startService(intent)
+        }
+    }
+
+    private fun startTasksList() {
+        val intent = Intent(this, TaskListActivity::class.java)
+        startActivity(intent)
     }
 
     private fun areLocationPermissionsGranted() : Boolean {
