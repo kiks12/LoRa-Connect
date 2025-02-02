8f6edaa2881fb638083d0ec0677521c52d998f67
Francis James Tolentino <francistolentino1107@gmail.com>
2025-02-02 23:20:36 +0800

    Created new function setNewTask and created new variable currentTask

--- android/app/src/main/java/com/lora_connect/application/map/MapViewModel.kt
+++ android/app/src/main/java/com/lora_connect/application/map/MapViewModel.kt
@@ -2,13 +2,19 @@ package com.lora_connect.application.map
 
 import android.annotation.SuppressLint
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asFlow
+import androidx.lifecycle.viewModelScope
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.Priority
 import com.google.android.gms.tasks.CancellationTokenSource
 import com.graphhopper.ResponsePath
+import com.lora_connect.application.room.entities.Task
+import com.lora_connect.application.tasks.current_task.CurrentTask
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.stateIn
 import org.maplibre.android.geometry.LatLng
 import org.maplibre.android.location.LocationComponentActivationOptions
 import org.maplibre.android.location.LocationComponentOptions
@@ -21,8 +27,10 @@ class MapViewModel(
     val buildLocationComponentActivationOptions: (style: Style, locationComponentOptions: LocationComponentOptions) -> LocationComponentActivationOptions,
     private val getRoute: (startLatitude: Double, startLongitude: Double, endLatitude: Double, endLongitude: Double) -> ResponsePath?
 ): ViewModel() {
+    private val currentTaskClass = CurrentTask.instance
     private val _state = MutableStateFlow(MapState())
     val state : StateFlow<MapState> = _state.asStateFlow()
+    val currentTask = currentTaskClass.getTask().asFlow().stateIn(viewModelScope, SharingStarted.Lazily, initialValue = null)
 
     fun setLatLng(latitude: Double, longitude: Double) {
         _state.value = _state.value.copy(
@@ -31,7 +39,7 @@ class MapViewModel(
         )
     }
 
-    fun setMarkerLatLng(latLng: LatLng) {
+    private fun setMarkerLatLng(latLng: LatLng) {
         _state.value = _state.value.copy(
             markerLatLng = latLng
         )
@@ -64,7 +72,12 @@ class MapViewModel(
         }
     }
 
-    fun createRoute() {
+    fun setNewTask(task: Task) {
+        setMarkerLatLng(LatLng(task.latitude!!.toDouble(), task.longitude!!.toDouble()))
+        createRoute()
+    }
+
+    private fun createRoute() {
         if (_state.value.markerLatLng == null) return
         val best = getRoute(_state.value.latitude, _state.value.longitude, _state.value.markerLatLng!!.latitude, _state.value.markerLatLng!!.longitude)
         _state.value = _state.value.copy(
