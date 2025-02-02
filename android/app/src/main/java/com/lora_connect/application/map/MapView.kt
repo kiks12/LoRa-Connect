287e6d9d9c3838607cb0ee825957ee56d3c549ac
Francis James Tolentino <francistolentino1107@gmail.com>
2025-02-02 23:21:01 +0800

    Used CurrentTaskCard

--- android/app/src/main/java/com/lora_connect/application/map/MapView.kt
+++ android/app/src/main/java/com/lora_connect/application/map/MapView.kt
@@ -7,8 +7,7 @@ import android.net.Uri
 import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -19,8 +18,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.lora_connect.application.permissions.RequestLocationPermissionUsingRememberLauncherForActivityResult
+import com.lora_connect.application.tasks.current_task.CurrentTaskCard
 import org.maplibre.android.annotations.MarkerOptions
 import org.maplibre.android.annotations.Polyline
 import org.maplibre.android.annotations.PolylineOptions
@@ -32,6 +33,7 @@ import org.maplibre.android.maps.Style
 @SuppressLint("MissingPermission")
 @Composable
 fun MapView(mapView: MapView, mapViewModel: MapViewModel) {
+    val currentTask by mapViewModel.currentTask.collectAsState()
     val state by mapViewModel.state.collectAsState()
     var polyline by remember {
         mutableStateOf<Polyline?>(null)
@@ -96,6 +98,10 @@ fun MapView(mapView: MapView, mapViewModel: MapViewModel) {
         }
     }
 
+    LaunchedEffect(currentTask) {
+        if (currentTask != null) mapViewModel.setNewTask(currentTask!!)
+    }
+
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomCenter) {
         AndroidView(
             factory = {
@@ -110,19 +116,20 @@ fun MapView(mapView: MapView, mapViewModel: MapViewModel) {
 
                     }
                     map.cameraPosition = org.maplibre.android.camera.CameraPosition.Builder().target(location = LatLng(state.latitude, state.longitude)).zoom(10.0).build()
-
-                    map.addOnMapClickListener {
-                        mapViewModel.setMarkerLatLng(LatLng(it.latitude, it.longitude))
-                        true
-                    }
+//                    map.addOnMapClickListener {
+//                        mapViewModel.setMarkerLatLng(LatLng(it.latitude, it.longitude))
+//                        true
+//                    }
                 }
 
                 mapView
             },
             modifier = Modifier.fillMaxSize()
         )
-        Button(onClick = mapViewModel::createRoute) {
-            Text(text = "Show Route")
+        if (currentTask != null) {
+            Box(modifier = Modifier.padding(8.dp)) {
+                CurrentTaskCard(task = currentTask!!)
+            }
         }
     }
 }
