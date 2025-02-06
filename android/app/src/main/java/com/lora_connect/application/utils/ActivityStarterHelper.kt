7aef7931b83e1b54bde7b80354184c3cf73c67be
Francis James Tolentino <francistolentino1107@gmail.com>
2025-02-06 23:51:45 +0800

    New function startMapActivity

--- android/app/src/main/java/com/lora_connect/application/utils/ActivityStarterHelper.kt
+++ android/app/src/main/java/com/lora_connect/application/utils/ActivityStarterHelper.kt
@@ -2,10 +2,18 @@ package com.lora_connect.application.utils
 
 import android.content.Context
 import android.content.Intent
+import com.lora_connect.application.map.MapActivity
 
 class ActivityStarterHelper(private val context: Context) {
     fun startActivity(activity: Class<*>) {
         val intent = Intent(context, activity)
+        context.startActivity(intent)
+    }
+
+    fun startMapActivity(deviceAddress: String) {
+        val intent = Intent(context, MapActivity::class.java).apply {
+            putExtra("DEVICE_ADDRESS", deviceAddress)
+        }
         context.startActivity(intent)
     }
 }
