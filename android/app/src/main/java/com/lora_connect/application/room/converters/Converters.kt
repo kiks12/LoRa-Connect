af31a074ce8830ea18d6103cc943b851f7517439
Francis James Tolentino <francistolentino1107@gmail.com>
2025-02-08 16:11:41 +0800

    New Converters for TaskStatus

--- android/app/src/main/java/com/lora_connect/application/room/converters/Converters.kt
+++ android/app/src/main/java/com/lora_connect/application/room/converters/Converters.kt
@@ -1,6 +1,7 @@
 package com.lora_connect.application.room.converters
 
 import androidx.room.TypeConverter
+import com.lora_connect.application.tasks.TaskStatus
 import java.util.Date
 
 class Converters {
@@ -12,5 +13,15 @@ class Converters {
     @TypeConverter
     fun dateToTimestamp(date: Date?) : Long? {
         return date.let { it?.time }
+    }
+
+    @TypeConverter
+    fun fromTaskStatus(status: TaskStatus) : String {
+        return status.name
+    }
+
+    @TypeConverter
+    fun toTaskStatus(statusString: String) : TaskStatus {
+        return TaskStatus.valueOf(statusString)
     }
 }
