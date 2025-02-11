2b71c95b49249f3e9df4dff330e987ff2b26ca62
Francis James Tolentino <francistolentino1107@gmail.com>
2025-02-11 22:53:10 +0800

    New Arduino Files

---
+++ arduino/LoRa-setup/LoRa-setup.ino
@@ -0,0 +1,14 @@
+#define RFM95_CS 10  // LoRa chip select pin
+#define RFM95_RST 9  // LoRa reset pin
+#define RFM95_INT 2  // LoRa interrupt pin
+#define RF95_FREQ 915.0 // LoRa frequency in MHz
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
