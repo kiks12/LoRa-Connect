a09743215eed5d188d582d2f9917e26093e3ea96
Francis James Tolentino <francistolentino1107@gmail.com>
2025-02-15 23:29:59 +0800

    Implemented Rescuers API GET rescuers without team

---
+++ web/app/api/rescuers/no-teams/route.ts
@@ -0,0 +1,36 @@
+
+import { getRescuersWithoutTeam } from "@/server/db/rescuers";
+import { internalServerErrorReturnValue, methodNotAllowed, prismaClientInitializationErrorReturnValue, prismaClientValidationErrorReturnValue } from "@/utils/api";
+import { PrismaClientInitializationError, PrismaClientValidationError } from "@prisma/client/runtime/library";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const rescuers = await getRescuersWithoutTeam()
+    return NextResponse.json({
+      rescuers
+    })
+  } catch (error) {
+    if (error instanceof PrismaClientInitializationError)
+      return prismaClientInitializationErrorReturnValue(error)
+    if (error instanceof PrismaClientValidationError)
+      return prismaClientValidationErrorReturnValue(error)
+    return internalServerErrorReturnValue(error)   
+  }
+}
+
+export async function POST() {
+  return methodNotAllowed({})
+}
+
+export async function PATCH() {
+  return methodNotAllowed({})
+}
+
+export async function PUT() {
+  return methodNotAllowed({})
+}
+
+export async function DELETE() {
+  return methodNotAllowed({})
+}
