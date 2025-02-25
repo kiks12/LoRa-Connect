9cfcdb47046521f3f4810e42d305cdd9b6035e0a
Francis James Tolentino <francistolentino1107@gmail.com>
2025-02-25 18:19:27 +0800

    Correct payload

--- python-scripts/payload.py
+++ python-scripts/payload.py
@@ -4,4 +4,4 @@ from tags import *
 
 
 def start_location_transmission_to_tru():
-    return f"{TO_ALL_USERS}${START_LOCATION_TRANSMISSION_TO_TRU}"
+    return f"{TO_ALL_USERS}{START_LOCATION_TRANSMISSION_TO_TRU}"
