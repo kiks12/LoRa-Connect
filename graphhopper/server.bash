da35aeb05193320a9622003dd1a446ce074d964e
Francis James Tolentino <francistolentino1107@gmail.com>
2025-03-02 16:38:06 +0800

    Obstacle handling

--- graphhopper/server.bash
+++ graphhopper/server.bash
@@ -1,7 +1,11 @@
 
 # UNCOMMENT TO PRODUCE ROUTING DATA FOR MOBILE APP
-#java -D"dw.graphhopper.datareader.file=philippines-latest-2.osm.pbf" -jar graphhopper-web-1.0.jar server config-example-v1.0.yml
+#java -D"dw.graphhopper.datareader.file=philippines-latest-2.osm.pbf" -jar graphhopper-web-1.0.jar server config-web-1.0.yml
 
 # COMMENT WHEN PRODUCING ROUTING DATA FOR MOBILE APP
-java -D"dw.graphhopper.datareader.file=philippines-latest-2.osm.pbf" -jar graphhopper-web-10.0.jar server config-example.yml
+# java -D"dw.graphhopper.datareader.file=philippines-latest-2.osm.pbf" -jar graphhopper-web-10.0.jar server config-web-10.0-v1.yml
+
+
+# trial with custom model
+java -D"dw.graphhopper.datareader.file=philippines-latest-2.osm.pbf" -jar graphhopper-web-10.0.jar server config-web-10.0-v1.yml
 
