ffeeb92246f0d93d7f96ca1bb21997db5f0fe113
Francis James Tolentino <francistolentino1107@gmail.com>
2025-03-07 21:22:34 +0800

    random

--- web/app/map/layout.tsx
+++ web/app/map/layout.tsx
@@ -7,27 +7,30 @@ import { MenuIcon } from "lucide-react";
 import { SideBar } from "./_components/Sidebar";
 import Map from "./_components/Map";
 import { MapProvider } from "@/contexts/MapContext";
+import { ObstaclesProvider } from "@/contexts/ObstacleContext";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<MapProvider>
 			<AppProvider>
-				<main className="flex relative">
-					<div className="flex-1 relative">
-						<div className="absolute w-screen flex justify-between p-2 z-50">
-							<div></div>
-							<Button size="icon" variant="outline" className="flex md:hidden">
-								<MenuIcon />
-							</Button>
+				<ObstaclesProvider>
+					<main className="flex relative">
+						<div className="flex-1 relative">
+							<div className="absolute w-screen flex justify-between p-2 z-50">
+								<div></div>
+								<Button size="icon" variant="outline" className="flex md:hidden">
+									<MenuIcon />
+								</Button>
+							</div>
+							<div className="flex items-center justify-center h-screen z-10 transition-transform transform">
+								<Map key={"main-map"} />
+							</div>
 						</div>
-						<div className="flex items-center justify-center h-screen z-10 transition-transform transform">
-							<Map key={"main-map"} />
+						<div className={`hidden md:block min-w-[512px] max-w-lg h-screen shadow-lg z-40`}>
+							<SideBar>{children}</SideBar>
 						</div>
-					</div>
-					<div className={`hidden md:block min-w-[512px] max-w-lg h-screen shadow-lg z-40`}>
-						<SideBar>{children}</SideBar>
-					</div>
-				</main>
+					</main>
+				</ObstaclesProvider>
 				<Toaster />
 			</AppProvider>
 		</MapProvider>
--- web/app/map/obstacles/page.tsx
+++ web/app/map/obstacles/page.tsx
@@ -1,13 +1,8 @@
 "use client";
 
-import { ObstaclesProvider } from "@/contexts/ObstacleContext";
 import { lazy } from "react";
 const ObstacleControls = lazy(() => import("../_components/ObstacleControls"));
 
 export default function ObstacleControlsPage() {
-	return (
-		<ObstaclesProvider>
-			<ObstacleControls />
-		</ObstaclesProvider>
-	);
+	return <ObstacleControls />;
 }
