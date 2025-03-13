9cda64a6dbb34e11d3adb4fef4010ccc6db8545b
Francis James Tolentino <francistolentino1107@gmail.com>
2025-03-13 22:27:42 +0800

    random

--- graphhopper/server.bash
+++ graphhopper/server.bash
@@ -9,4 +9,5 @@
 # FINAL 
 #java -D"dw.graphhopper.datareader.file=philippines-latest-2.osm.pbf" -jar graphhopper-web-10.0.jar server config-web-10.0-v1.yml
 
-java -D"dw.graphhopper.datareader.file=philippines-latest-2.osm.pbf" -jar graphhopper-web-10.0.jar server config-web-10.0-v1.yml
+# FOR TESTING 
+java -D"dw.graphhopper.datareader.file=philippines-latest-2.osm.pbf" -jar graphhopper-web-10.0.jar server config-web-10.0-v2.yml
