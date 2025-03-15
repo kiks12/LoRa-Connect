f61ede2a19c3e364fe8ff10feded911bb31f9fd7
Francis James Tolentino <francistolentino1107@gmail.com>
2025-03-16 00:47:42 +0800

    home page revamped

--- web/app/_components/HomeCard.tsx
+++ web/app/_components/HomeCard.tsx
@@ -7,7 +7,7 @@ import { ReactNode } from "react";
 export default function HomeCard({ subtitle, title, link, icon }: { subtitle?: string; title: string; link: string; icon?: ReactNode }) {
 	return (
 		<Link href={link}>
-			<Card className="shadow-md hover:shadow-lg border-none justify-center items-center px-4 py-8 min-h-[215px]">
+			<Card className="shadow-md hover:shadow-2xl border-white/40 hover:bg-white/70 hover:border-white justify-center rounded-2xl items-center px-4 py-8 min-h-[215px] bg-white/10 backdrop-blur-3xl transition">
 				<CardContent className="pt-4">
 					<div className="flex flex-col items-center justify-center">
 						{icon && <div>{icon}</div>}
--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,22 +1,37 @@
 "use client";
 
-import { lazy } from "react";
+import { lazy, useMemo } from "react";
 import { Container } from "./components/Container";
 import HOME_LINKS from "./links";
 
 const HomeCard = lazy(() => import("./_components/HomeCard"));
 
 export default function Home() {
+	const date = useMemo(() => {
+		return new Date();
+	}, []);
+
 	return (
-		<main className="bg-gradient-to-br from-gray-50 to-gray-200 min-h-screen">
-			<Container className="pt-10">
-				<h1 className="text-2xl font-semibold tracking-tight">LoRa-Connect Control Center</h1>
-				<div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-					{HOME_LINKS.map((value, index) => {
-						return <HomeCard title={value.title} link={value.link} key={index} icon={value.icon} subtitle={value.subtitle} />;
-					})}
-				</div>
-			</Container>
+		<main className="bg-gradient-to-br from-white to-slate-300 min-h-screen bg-[url(/bg.jpg)] bg-cover">
+			<div className="flex items-center justify-center min-h-screen min-w-screen bg-white/30 backdrop-blur-2xl">
+				<Container className="pt-10 min-h-screen">
+					<div className="flex justify-between items-end">
+						<div className="">
+							<h1 className="text-3xl font-semibold tracking-normal">LoRa-Connect</h1>
+							<h2 className="text-xl font-medium">Control Center</h2>
+						</div>
+						<div className="flex flex-col justify-end items-end">
+							<p>{date.toDateString()}</p>
+							<p>{date.toTimeString()}</p>
+						</div>
+					</div>
+					<div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1">
+						{HOME_LINKS.map((value, index) => {
+							return <HomeCard title={value.title} link={value.link} key={index} icon={value.icon} subtitle={value.subtitle} />;
+						})}
+					</div>
+				</Container>
+			</div>
 		</main>
 	);
 }
